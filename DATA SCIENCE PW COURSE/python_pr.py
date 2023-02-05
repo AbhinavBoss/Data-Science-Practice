49eced46ebf4b3cad78c79ae93f7e84b3f07652d
Abhinav Varshney <varshneyabhinav66@gmail.com>
2023-02-05 21:14:39 +0530

    mza aa gya bidu

--- DATA SCIENCE PW COURSE/python_pr.py
+++ DATA SCIENCE PW COURSE/python_pr.py
@@ -3,8 +3,12 @@
 # list2=[i**3 for i in list1]
 # print([i for i in range(5)])
 
-c_to_f=lambda c:(9/5) * c + 32
-print(c_to_f(-40))
-n=5
-a=lambda fact: fact*i for i in range(1,n)
-print(a(5))
+# c_to_f=lambda c:(9/5) * c + 32
+# print(c_to_f(-40))
+
+a=[1,2,3]
+b=["Abhinav","Akshat","Shrinu"]
+dic={}
+for i in range(3):
+    dic[a[i]]=b[i]
+print(dic)
