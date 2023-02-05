7535ed2b2d34f026bc62b86a1466c3344c61ea71
Abhinav Varshney <varshneyabhinav66@gmail.com>
2023-02-05 11:41:38 +0530

    bas ese hi

--- DATA SCIENCE PW COURSE/python_pr.py
+++ DATA SCIENCE PW COURSE/python_pr.py
@@ -2,8 +2,9 @@
 # list1=[1,2,3,4]
 # list2=[i**3 for i in list1]
 # print([i for i in range(5)])
-a=[None]*5
-a[2],a[3]=5,True
-print(a)
+
 c_to_f=lambda c:(9/5) * c + 32
 print(c_to_f(-40))
+n=5
+a=lambda fact: fact*i for i in range(1,n)
+print(a(5))
